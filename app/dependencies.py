00b250a35fdec71e22651063a01d5ee259138974
Monica Erica Sardena <monicaerica.sardena@gmail.com>
2025-11-26 21:44:59 +0100

    added dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,15 +1,25 @@
 from app.clients.pokeapi_client import PokeAPIClient
 from app.clients.translation_client import TranslationClient 
 from app.services.pokemon_service import PokemonService
+from fastapi import Depends
+
+_poke_client = None
+_translation_client = None
 
 def get_poke_client() -> PokeAPIClient:
-    return PokeAPIClient()
+    global _poke_client
+    if _poke_client is None:
+        _poke_client = PokeAPIClient()
+    return _poke_client
 
-def get_translation_client() -> TranslationClient: 
-    return TranslationClient()
+def get_translation_client() -> TranslationClient:
+    global _translation_client
+    if _translation_client is None:
+        _translation_client = TranslationClient()
+    return _translation_client
 
 def get_pokemon_service(
-    poke_client: PokeAPIClient = get_poke_client,
-    translation_client: TranslationClient = get_translation_client, 
+    poke_client: PokeAPIClient = Depends(get_poke_client),
+    translation_client: TranslationClient = Depends(get_translation_client), 
 ) -> PokemonService:
     return PokemonService(poke_client=poke_client, translation_client=translation_client)
